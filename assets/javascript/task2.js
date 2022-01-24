85c81df7cd0bc2aba8f6d301193eedcde731a210
admin <cookieking524@gmail.com>
2022-01-24 09:54:31 -0500

    bye

--- assets/javascript/task2.js
+++ assets/javascript/task2.js
@@ -1,7 +1,20 @@
 /* Put code for the task below */
 
 function red() {
-    var redbutt = document.getElementById("buttonred")
+    var redbutt = document.getElementById("nametable")
 
     redbutt.style.backgroundColor = "red"
 }
+
+function blue() {
+    var blackbutt = document.getElementById("nametable")
+
+    blackbutt.style.backgroundColor = "blue"
+    
+}
+function yellow() {
+    var yellowbutt = document.getElementById("nametable")
+
+    yellowbutt.style.backgroundColor = "yellow"
+}
+
--- assets/javascript/task3.js
+++ assets/javascript/task3.js
@@ -1,1 +1,5 @@
-/* Put code for the task below */
+function magic() {
+    var pele = document.getElementById("upgraphya")
+
+    pele.innerHTML = "Hey you clicked that button!"
+}
